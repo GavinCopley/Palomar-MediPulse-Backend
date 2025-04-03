17a40fa987b70dd8876fc03c2ed51f763b0469fc
Ellioty15 <elliotyschool@gmail.com>
2025-04-03 09:25:45 -0700

    datacleaning, data name change

---
+++ data/datacleaning.py
@@ -0,0 +1,20 @@
+# Read the JSON file and convert it to a Pandas DataFrame 
+# pd.read_json:  a method that reads a JSON and converts it to a DataFrame (df)
+# df: a variable that holds the DataFrame
+df = pd.read_json('data/grade.json')
+
+# Print the DataFrame
+print(df)
+
+# Additional print statements to understand the DataFrame:
+# print(df.info()) # prints a summary of the DataFrame, simmilar to database schema
+# print(df.describe()) # prints statistics of the DataFrame
+# print(df.head()) # prints the first 5 rows of the DataFrame
+# print(df.tail()) # prints the last 5 rows of the DataFrame
+# print(df.columns) # prints the columns of the DataFrame
+# print(df.index) # prints the index of the DataFrame
+
+# Questions:
+# What part of the data set needs to be cleaned?
+# From PBL learning, what is a good time to clean data?  
+# Could you hav Garbage in, Garbage out problem if you don't clean the data?
